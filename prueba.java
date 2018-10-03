361b57413cd4740b055b100c2d1d2e9a1b0cb856
Alejandro Salagre Vara <alejandrosv3@gmail.com>
2018-10-03 12:38:57 +0200

    muestra la ciudad donde resido actualmente

--- prueba.java
+++ prueba.java
@@ -1,6 +1,7 @@
-pubic class Prueba{
+public class prueba{
  public static void main(String args[]){
   System.out.println("Alejandro Salagre Vara");
   System.out.println("Benavente");
+  System.out.println("Leon");
  }
 }
